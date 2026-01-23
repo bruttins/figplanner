b0da91ed70b4e0b09500be904acc428554264cf7
bruttins <bruttins@gmail.com>
2026-01-23 18:22:53 +0100

    finished pseudocode rotation

--- src/rotation.py
+++ src/rotation.py
@@ -16,42 +16,35 @@ def create_rotation_table(names):
 	return rounds
 
 def create_rotation_4(names):
-	#helper function for 4
-	pass
+	"""helper function for 4
+	1. create variables for each role (indices 0-3 of names)
+	2. create first round-dict and append to rounds
+	3. loop over names-list (starting with index1, since first round is already done)
+		store_h1 = current_helper1
+		current_helper1 = current_helper2
+		current_helper2 = observer
+		observer = trainee
+		trainee = store_h1
+	4. create new_round-dict with current-variables
+	5. append new_round to rounds
+	"""
 
 def create_rotation_with_idle(names):
 	"""helper function for 5-7
-	0. create count-dictionary (as in test_rotation.py)
 	1. Create variables for each role (indices 0-3 of names) plus idle-list (5:) 
-	2. loop over names-list
-		3. assign trainee: loop over name in idle-list
-			if name's trainee-count is 0: 
-				assign name to trainee-variable
-				pop name from idle-list
-		if no idle is suitable: 
-			if helper1's trainee-count == 0:
-				current_trainee = helper1
-			else if helper2's trainee-count == 0:
-				current_trainee = helper2
-			else:
-				current_trainee = observer
-		4. assign helpers and observer: if helper1's helper-count < 3: 
-				helper1 stays
-				append helper2 to idle
-				current_helper2 = observer
-				current_observer = idle[0]
-				pop idle[0]
-			else: 
-				current_helper1 = observer
-				if current_helper2's helper-count < 3:
-					helper2 stays
-					current_observer = idle[0]
-					pop idle[0]
-				else:
-					append current_helper2 to idle
-					current_helper2 = idle[0]
-					pop idle[0]
-					observer stays
-		5. create new_round-dictionary with current-variables
-		6. append new_round to rounds
+	2. create first round-dict and append to rounds
+	3. loop over names-list (starting with index 1, since first round is already done)
+		if i = 1, 3, 5 or 7:
+			idle.append(current_helper1)
+			current_helper1 = observer
+			observer = current_trainee
+			current_trainee=idle[0]
+			idle.pop(0)
+		else (means i= 2, 4 or 6):
+			idle.append(current_helper2)
+			current_helper2 = current_trainee
+			current_trainee=idle[0]
+			idle.pop(0)
+	5. create new_round-dictionary with current-variables
+	6. append new_round to rounds
 		"""

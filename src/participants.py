19f57a28a54fdd49ee2d67547597fb5862cc9a36
bruttins <bruttins@gmail.com>
2026-02-03 15:56:18 +0100

    update last changes

--- src/participants.py
+++ src/participants.py
@@ -1,4 +1,14 @@
-"""
-1. prompt for participants
-2. return list
-"""
+
+# 1. prompt for participants (all at once, separated by commas)
+# 2. check for possible wrong input-structure:
+#     a. left out comma
+#     b. wrong separation (semicolon, period)
+#  2.5 keep prompting until valid
+# 3. Turn Input-String into List
+# 4. check List for possible invalid inputs:
+#     a. less than 4 names
+#     b. more than 7 names
+#     c. duplicates
+#  4.5 repeat 2-4 until input-list is valid.
+# 5. Shuffle List
+# 6. return shuffled list
